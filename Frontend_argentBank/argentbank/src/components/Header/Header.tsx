124c29731a6fcf35351c28219247053e0b92ca5c
Heihachi <heiariim1@gmail.com>
2024-06-21 09:27:22 -1000

    🔥 Header> CHANGE Header component logic

--- Frontend_argentBank/argentbank/src/components/Header/Header.tsx
+++ Frontend_argentBank/argentbank/src/components/Header/Header.tsx
@@ -1,30 +1,53 @@
-import { Link } from 'react-router-dom';
-import logo from "../../assets/images/argentBankLogo.png";
+import { NavLink } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
+import { logOut } from '../../app/user.actions/actions';
+import { useDispatch, useSelector } from 'react-redux';
+import Logo from "../../assets/images/argentBankLogo.png";
 
-export const Header = () => {
-  return (
+export const Header:React.FC = () => {
+   
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+    const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+    const user = useSelector((state) => state.auth.user);
+  
+    const handlelogOut = (e) => {
+      e.preventDefault();
+      dispatch(logOut());
+      navigate('/');
+    };
 
-    <>
-      <nav className="main-nav">
-        <Link className="main-nav-logo" to="/">
-          <img
-            className="main-nav-logo-image"
-            src={logo}
-            alt="Argent Bank Logo"
-          />
-          <h1 className="sr-only">Argent Bank</h1>
-        </Link>
-        <div>
-          <Link className="main-nav-item" to="/user">
-            <i className="fa fa-user-circle"></i>
-            Tony
-          </Link>
-          <Link className="main-nav-item" to="/">
-            <i className="fa fa-sign-out"></i>
-            Sign Out
-          </Link>
-        </div>
-      </nav>
-    </>
-  )
-}
+    return (
+        <nav>
+            <NavLink to='/'>
+                <img src={Logo} alt='Argent Bank Logo' />
+                <h1 className='sr-only'>Argent Bank</h1>
+            </NavLink>
+            <ul>
+                <li>
+                    {isAuthenticated ? (
+                        <>
+                            <NavLink to='/profile'>
+                                <i className='fa fa-user-circle main-nav-item'></i>
+                                {user?.userName}
+                            </NavLink>
+                            <NavLink to='/' onClick={handlelogOut}>
+                                <i className='fa fa-sign-out'></i>
+                                Sign Out
+                            </NavLink>
+                        </>
+                    ) : (
+                        <NavLink to='/login'>
+                            <i className='fa fa-user-circle'></i>
+                            Sign In
+                        </NavLink>
+                    )}
+                </li>
+            </ul>
+        </nav>
+    );
+};
+
+
+
+
